a00fc752d37ef4c6215682792cd54dfc36ef3298
VanwTus09 <tunguyen/05112004@gmail.com>
2025-07-16 23:43:45 +0700

    fix conflic ab emoji

--- my-app/src/components/emoji-picker.tsx
+++ my-app/src/components/emoji-picker.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import data from "@emoji-mart/data";
-import Picker from "@emoji-mart/react";
 import { Smile } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import EmojiPickerComponent, { Theme } from "emoji-picker-react";
 
 interface EmojiPickerProps {
   onChange: (value: string) => void;
@@ -15,18 +14,20 @@ const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
 
   return (
     <Popover>
-      <PopoverTrigger>
-        <Smile className="cursor-pointer text-zinc-500 transition hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300" />
+      <PopoverTrigger asChild>
+        <button type="button">
+          <Smile className="cursor-pointer text-zinc-500 transition hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300" />
+        </button>
       </PopoverTrigger>
       <PopoverContent
         side="right"
         sideOffset={40}
         className="mb-16 border-none bg-transparent shadow-none drop-shadow-none"
       >
-        <Picker
-          theme={resolvedTheme}
-          data={data}
-          onEmojiSelect={(emoji: { native: string }) => onChange(emoji.native)}
+        <EmojiPickerComponent
+          theme={resolvedTheme as Theme}
+          onEmojiClick={(emojiObject) => onChange(emojiObject.emoji)}
+          height={350}
         />
       </PopoverContent>
     </Popover>
