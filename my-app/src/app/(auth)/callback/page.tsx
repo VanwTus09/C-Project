ded823c1764ab39c98205e72a3e5aa300670f283
VanwTus09 <tunguyen/05112004@gmail.com>
2025-07-30 21:28:47 +0700

    fix login

--- my-app/src/app/(auth)/callback/page.tsx
+++ my-app/src/app/(auth)/callback/page.tsx
@@ -8,13 +8,6 @@ import { uploadToCloudinaryFromUrl } from "@/lib/cloudinary";
 export default function AuthCallbackPage() {
   const router = useRouter();
   useEffect(() => {
-  const code = new URLSearchParams(window.location.search).get('code');
-  if (code) {
-    supabase.auth.exchangeCodeForSession(code);
-  }
-}, []);
-
-  useEffect(() => {
     const handleAuthRedirect = async () => {
       const { data: sessionData } = await supabase.auth.getSession();
       const session = sessionData?.session;
